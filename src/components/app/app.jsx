fa2450cdd99a448e59e39511858210be02036c60
Eugene Karpov <webmaster.karpov@yandex.ru>
2020-02-12 15:07:16 +0300

    module3-task1: react router added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import GuessArtist from '../guess-artist/guess-artist.jsx';
+import GuessGenre from '../guess-genre/guess-genre.jsx';
 
 class App extends React.PureComponent {
   constructor(props) {
@@ -13,7 +16,19 @@ class App extends React.PureComponent {
     } = this.props;
 
     return (
-      <WelcomeScreen {...params.welcome} />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen {...params.welcome} />
+          </Route>
+          <Route exact path="/dev-artist">
+            <GuessArtist />
+          </Route>
+          <Route exact path="/dev-genre">
+            <GuessGenre />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
